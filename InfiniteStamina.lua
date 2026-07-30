993486708d4224920e6eb87a2a412b61cc0986cc
MinecartMoving <minecartmovin@gmail.com>
2026-07-29 19:37:16 -0500

    Delete InfiniteStamina.lua

--- InfiniteStamina.lua
+++
@@ -1,9 +0,0 @@
-while wait() do
-    local Sprinting = game:GetService("ReplicatedStorage").Systems.Character.Game.Sprinting
-    local stamina = require(Sprinting)
-    stamina.MaxStamina = 100  -- Maximum stamina
-    stamina.MinStamina = -20  -- Minimum stamina
-    stamina.StaminaGain = 100 -- Stamina gain
-    stamina.StaminaLoss = 5 -- Stamina loss
-    stamina.StaminaLossDisabled = true -- Disable stamina drain (true/false)
-end
