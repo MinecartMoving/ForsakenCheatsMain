5e029f8bba00025df03efd72b38f483f4f780d65
MinecartMoving <minecartmovin@gmail.com>
2026-07-29 19:02:41 -0500

    Update SurvivorESP.lua

--- SurvivorESP.lua
+++ SurvivorESP.lua
@@ -1,7 +1,7 @@
 workspace.Players.Survivors.ChildAdded:Connect(function(child)
 
 local Highlight = Instance.new("Highlight")
-Highlight.TintColor = Color3.new(1, 1, 1)
+Highlight.FillColor = Color3.new(1, 1, 1)
 Highlight.Parent = Child
 
 end)
